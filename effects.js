865aa267340069718c959a017724eb5a76f1d07c
Vince <vincentjvillacorta@gmail.com>
2020-09-21 19:10:24 -0700

    Semi-Presentable. Addes Images and BS4 install.

--- effects.js
+++ effects.js
@@ -1,8 +1,4 @@
 $(document).ready(function() {
- // executes when HTML-Document is loaded and DOM is ready
-console.log("document is ready");
-
-
   $( ".card" ).hover(
   function() {
     $(this).addClass('shadow-lg').css('cursor', 'pointer');
@@ -11,5 +7,4 @@ console.log("document is ready");
   }
 );
 
-// document ready
 });
